d14b0bfa699dd6b40d5b039969c5f886d946b574
redbossrabbit <57042774+redbossrabbit@users.noreply.github.com>
2020-02-26 16:34:33 +0100

    Update fxApi.js

--- javascript/fxApi.js
+++ javascript/fxApi.js
@@ -7,15 +7,16 @@
 //         show.innerHTML = data.rates.USD
 //     })
 const metrics = document.getElementById('metrics');
-const mY = metrics.scrollHeight + 5000;
 const ACTION = () => {
-    if (window.pageYOffset > 1200 && window.pageYOffset < 1250) {
+    if (window.pageYOffset > 1150 && window.pageYOffset < 1250) {
         const aotVal = document.getElementById('aot-val');
         const notVal = document.getElementById('not-val');
         const csVal = document.getElementById('cs-val');
-        const aotNums = ['120', '345', '894', '11K', '21K', '32K', '20M', '33M', '99M', '1B'];
-        const notNums = ['120', '345', '894', '11K', '21K', '32K', '50K', '99K', '111K', '600K'];
-        const csNums = ['120', '345', '894', '11K', '21K', '32K', '70K', '111K', '77K', '1M'];
+        const yVal = document.getElementById('y');
+        const aotNums = ['120', '345', '894', '11K', '21K', '32K', '20M', '33M', '99M', '$5B+'];
+        const notNums = ['120', '345', '894', '11K', '21K', '32K', '50K', '99K', '111K', '1M+'];
+        const csNums = ['120', '345', '894', '11K', '21K', '32K', '70K', '111K', '77K', '100K+'];
+        const yNums = ['1', '2', '3', '4', '5', '7', '9', '11', '13', '14'];
         let b = 0;
         const run = setInterval(() => {
             if (b === aotNums.length - 1) {
@@ -39,6 +40,14 @@ const ACTION = () => {
             }
             csVal.innerHTML = csNums[d];
             d++;
+        }, 100);
+        let e = 0;
+        const run4 = setInterval(() => {
+            if (e === yNums.length - 1) {
+                clearInterval(run4);
+            }
+            yVal.innerHTML = yNums[e];
+            e++;
         }, 100);
     } else {
         return;
